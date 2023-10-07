4ee457c8ba58cf8e51565ed8a5d3eb71f89d500e
Mitchell Sharma <sharmamitch@gmail.com>
2023-10-07 13:19:43 -0500

    Corrected projects section id and styling tags Minor styling adjustments

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ hamburger.addEventListener('click', () => {
 document.addEventListener('scroll', () => {
 	var scroll_position = window.scrollY;
 	if (scroll_position > 90) {
-		header.style.backgroundColor = 'black';
+		header.style.backgroundColor = '#171717';
 	} else {
 		header.style.backgroundColor = 'transparent';
 	}
